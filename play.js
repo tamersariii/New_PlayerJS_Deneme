6cc79a76eb0d9afebcbcef07033f5bbfaa59449b
Tamer SARI <tamersariii@outlook.com>
2025-01-04 21:48:43 +0300

    Update and rename play.js to players.js

--- play.js
+++
@@ -1,5 +0,0 @@
-[{"title":"1","poster":"https://github.com/tv-logo/tv-logos/blob/main/countries/turkey/show-tr.png","file":"https://ciner-live.daioncdn.net/showtv/showtv_1080p.m3u8"},
-{"title":"2","file":"https://dogus-live.daioncdn.net/tlc/tlc.m3u8"},
-{"title":"3","file":"https://dogus-live.daioncdn.net/dmax/dmax.m3u8"},
-{"title":"4","file":"https://turkmedya-live.ercdn.net/tv360/tv360_720p.m3u8"},
-{"title":"5","file":"https://beyaztv-live.daioncdn.net/beyaztv/beyaztv_1080p.m3u8"}]
---
+++ players.js
@@ -0,0 +1,5 @@
+[{"title":"1","file":"https://ciner-live.daioncdn.net/showtv/showtv_1080p.m3u8"},
+{"title":"2","file":"https://dogus-live.daioncdn.net/tlc/tlc.m3u8"},
+{"title":"3","file":"https://dogus-live.daioncdn.net/dmax/dmax.m3u8"},
+{"title":"4","file":"https://turkmedya-live.ercdn.net/tv360/tv360_720p.m3u8"},
+{"title":"5","file":"https://beyaztv-live.daioncdn.net/beyaztv/beyaztv_1080p.m3u8"}]
